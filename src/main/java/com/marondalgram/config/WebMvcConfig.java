4cb3f1bdeb580e1c051d4d5002f18967210d43ed
koreanzombie <ykh6chik5@gmail.com>
2021-08-20 18:40:28 +0900

    210820 두번째 데이터 등록

--- src/main/java/com/marondalgram/config/WebMvcConfig.java
+++ src/main/java/com/marondalgram/config/WebMvcConfig.java
@@ -3,6 +3,7 @@ package com.marondalgram.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.marondalgram.interceptor.PermissionInterceptor;
@@ -17,6 +18,11 @@ public class WebMvcConfig implements WebMvcConfigurer {
 		registry.addInterceptor(permissionInterceptor)
 		.addPathPatterns("/**")
 		.excludePathPatterns("/user/sign_out", "/static/**", "/error");
+	}
 	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/images/**")	// http://localhost/images/qwer_1629441269853/green.jpg
+		.addResourceLocations("file:///C:\\Users\\ganjinam\\Desktop\\class\\6_spring_project\\quiz\\marondalgram_workspace\\Marondalgram\\images/");	// 실제파일위치
 	}
 }
